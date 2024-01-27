19f8b9b5ccee62810f085a02a45991255a32156a
okorieonyedikachi <cosmasdaniella@gmail.com>
2024-01-27 05:23:20 +0100

    Feat: Data Table

--- components/TaskListContent.tsx
+++ components/TaskListContent.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { TodoTable } from "./DataTable";
 
 const TaskListContent = () => {
   return (
-    <div className='bg-white shadow-lg w-11/12 mx-auto mt-6'>
-        dfuhvsoj
+    <div className="bg-white shadow-lg w-11/12 mx-auto mt-6 px-6">
+      <TodoTable />
     </div>
-  )
-}
+  );
+};
 
-export default TaskListContent
+export default TaskListContent;
