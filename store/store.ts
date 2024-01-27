2963c79b161338696a66bb14a844f559d2bdcb28
okorieonyedikachi <cosmasdaniella@gmail.com>
2024-01-27 05:26:51 +0100

    File: redux store file

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from '@/store/task/taskSlice'
+import { persistReducer,  persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+ 
+ 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, taskReducer)
+
 
 export const store = configureStore({
-  reducer: {task:taskReducer},
+  reducer: persistedReducer,
 })
-
+export const persistor = persistStore(store)
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 
