dcd981056b3ea110e8b522962934344989580ba5
okorieonyedikachi <cosmasdaniella@gmail.com>
2024-01-28 05:57:14 +0100

    removed task card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import TaskCard from "@/components/TaskCard";
 import { Button } from "@/components/ui/button";
 import AddNewTask from "@/components/AddNewTask"
 import TaskListContent from "@/components/TaskListContent";
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <div className="bg-[#F7F9FF] w-screen h-screen flex flex-col">
       <Header/>
-      <TaskCard/>
       <TaskListContent/>
     </div>
   );
