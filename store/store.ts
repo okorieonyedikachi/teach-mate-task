aa0e21b76ee4eb09a8eac312aa1b790a3492856b
okorieonyedikachi <cosmasdaniella@gmail.com>
2024-01-28 05:58:07 +0100

    added new reducers and update store

--- store/store.ts
+++ store/store.ts
@@ -1,22 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
-import taskReducer from '@/store/task/taskSlice'
-import { persistReducer,  persistStore} from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
- 
- 
+import taskReducer from '@/store/task/taskSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 const persistConfig = {
   key: 'root',
   storage,
-}
- 
-const persistedReducer = persistReducer(persistConfig, taskReducer)
-
+};
 
 export const store = configureStore({
-  reducer: persistedReducer,
-})
-export const persistor = persistStore(store)
+  reducer: {
+    task: persistReducer(persistConfig, taskReducer),
+  },
+});
+export const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
--- store/task/taskSlice.ts
+++ store/task/taskSlice.ts
@@ -1,39 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import data from "@/data.json"
-import { Task } from '@/types'
+import data from '@/data.json';
+import { Task } from '@/types';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 export interface TaskState {
-    tasks: Task[]
-  }
-
-const initialState:TaskState = {
-  tasks:[]
-  // tasks:data.tasks.map(t=>({...t,date:new Date()}))
+  tasks: Task[];
 }
+
+const initialState: TaskState = {
+  tasks: data.tasks.map(t => ({
+    ...t,
+    date: new Date().toISOString(),
+    id: Math.random().toString(36),
+  })),
+};
 
 export const taskSlice = createSlice({
   name: 'task',
   initialState,
   reducers: {
-   
+    editTask: (state, action: PayloadAction<Task>) => {
+      const newTask = action.payload;
+      state.tasks = state.tasks.map(task =>
+        task.id === newTask.id ? newTask : task,
+      );
+    },
     addNewTask: (state, action: PayloadAction<Task>) => {
-      state.tasks.push(action.payload)
+      state.tasks.push(action.payload);
     },
     deleteTask: (state, action: PayloadAction<Task['id']>) => {
       const taskId = action.payload;
-      state.tasks = state.tasks.filter((task) => task.id !== taskId);
+      state.tasks = state.tasks.filter(task => task.id !== taskId);
     },
-    // deleteTask: (state, action: PayloadAction<Task['id']>) => {
-    //   const taskIndex = state.tasks.findIndex((task) => task.id === action.payload);
-    //   if (taskIndex !== -1) {
-    //     state.tasks.filter(taskIndex, 1);
-    //   }
-    // },
+    deleteMultipleTasks: (state, action: PayloadAction<Task['id'][]>) => {
+      const taskIds = action.payload;
+      state.tasks = state.tasks.filter(task => !taskIds.includes(task.id));
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const taskActions = taskSlice.actions
+export const taskActions = taskSlice.actions;
 
-export default taskSlice.reducer
+export default taskSlice.reducer;
